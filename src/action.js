b29845ca68d850342500f4c88133717257d513f7
theaveasso-dpc <sotheaveas.huort@dpc-group.com>
2022-10-25 10:58:14 +0700

    fixed

--- src/action.js
+++ src/action.js
@@ -9,10 +9,11 @@ const run = async () => {
 	const { context = {} } = github;
 	const { pull_request } = context.payload;
 
-	await octokit.issues.createComment({
+	await octokit.rest.issues.createComment({
 		...context.repo,
-		issue_number: pull_request.issue_number,
-		body: 'Thank you for submitting a pull request! We will try to rebuild this as soon as possible',
+		issue_number: pull_request.number,
+		body: `Thank you for submitting a pull request! We will
+					try to review this as soon as we can.`,
 	});
 };
 
