995693e8971b1ab01e8d836b50981b90301d7d68
theaveasso-dpc <sotheaveas.huort@dpc-group.com>
2022-10-25 10:33:53 +0700

    saying thank you to new pull requests

--- src/action.js
+++ src/action.js
@@ -1,5 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 const run = async () => {
-	console.log('Thank you');
+	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+	const octokit = github.getOctokit(GITHUB_TOKEN);
+
+	const { context = {} } = github;
+	const { pull_request } = context.payload;
+
+	await octokit.issues.createComment({
+		...context,
+		repo,
+		issue_number: pull_request.issue_number,
+		body: 'Thank you for submitting a pull request! We will try to rebuild this as soon as possible',
+	});
 };
 
 run();
